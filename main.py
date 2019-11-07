1aaf2757d62721d5a6881387f5149faa4daa7ad9
Tristan Trim <tTrim@linamar.com>
2019-11-07 15:47:34 -0500

    non worky working state

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 
 from gym import Env, spaces
 
-
-simpleTestEdges = (
-        (1,3,2),
-        (2,3,2),
-        (2,4,3),
-        (3,5,1),
-        (4,5,2),
-        )
+# (nodeIdA,nodeIdB,timeDistanceBetween)
+# A < B .`. there is no duplication
+simpleTestGraph = {
+        "N":5,
+        "E":(
+            (1,3,2),
+            (2,3,2),
+            (2,4,3),
+            (3,5,1),
+            (4,5,2),
+            ),
+        "maxE":3,
+        }
 
 class Truckenv(Env):
     """
     Trucks! Gotta have em, gotta send em on jobs!
+
+    The main API methods that users of this class need to know are:
+        step
+        reset
+        render
+        close
+        seed
+    And set the following attributes:
+        action_space: The Space object corresponding to valid actions
+        observation_space: The Space object corresponding to valid observations
+        reward_range: A tuple corresponding to the min and max possible rewards
+    Note: a default reward range set to [-inf,+inf] already exists. Set it if you want a narrower range.
     """
-    def __init__(slef,trucks=1,jobs=3,edges=simpleTestEdges):
+    def __init__(self,trucks=1,jobs=3,graph=simpleTestGraph):
+
+        self.nTrucks=trucks
+        self.nJobs=jobs
+        self.graph=graph
+
+        self.action_space = spaces.MultiDiscrete((self.graph["maxE"]+3,)*self.nTrucks)
+        self.observation_space = spaces.MultiDiscrete((self.graph["N"],)*self.nTrucks + (self.graph["N"],self.graph["N"])*self.nJobs)
+
+        self.trucks=nuply array of a good size, not too big, don't want to be wasteful 'eh?
+
+    def step(self,action):
+
 
 if (__name__=="__main__"):
     #env = gym.make("trucks-v0")
